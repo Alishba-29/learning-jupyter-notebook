a72dc21a3b257dde9583c439fe535c31ceae142b
Alishba-29 <alishba1429@gmail.com>
2024-09-03 15:20:27 +0500

    update

---
+++ first-jupyter-notebook/main.py
@@ -0,0 +1,10 @@
+name:str="alishba"
+
+#object.method()
+#object.attribute
+print(name.capitalize())
+print(name.casefold())
+print(name.center(25,"*"))
+print(name.upper())
+print(name.lower())
+print(name.title())
